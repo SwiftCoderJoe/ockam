4105ab19ff38739f94ebed341622d2253e90bc4b
etorreborre <etorreborre@yahoo.com>
2023-10-13 12:50:48 +0200

    feat(rust): add in-memory database support

--- implementations/rust/ockam/ockam_identity/src/purpose_keys/storage/mod.rs
+++ implementations/rust/ockam/ockam_identity/src/purpose_keys/storage/mod.rs
@@ -1,5 +1,5 @@
-mod purpose_keys_repository_impl;
+mod purpose_keys_repository_sql;
 mod purpose_keys_repository_trait;
 
-pub use purpose_keys_repository_impl::*;
+pub use purpose_keys_repository_sql::*;
 pub use purpose_keys_repository_trait::*;
